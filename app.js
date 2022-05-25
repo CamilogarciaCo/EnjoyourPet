a955ca7518a14be1a1bd438514fbdaa474f8b8ef
CamilogarciaCo <camilogarcia1372@gmail.com>
2022-05-25 10:54:05 -0500

    Agregamos proyecto

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = process.env.PORT || 3000;
+app.use(express.static("public"));
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.set("view engine", "ejs")
+
+app.listen(port, () => {
+  console.log("Levantando el Servidor");
+});
+
+// rutas
+app.get("/", (req, res) => {
+    res.render("index")
+  });
+
+app.get('/register', (req, res) => {
+  res.render("register")
+})
+/*
+app.get("/nosotros", (req, res) => {
+    res.render("nosotros")
+})
+app.get("/contactanos", (req, res) => {
+  res.render("contactanos")
+})*/
